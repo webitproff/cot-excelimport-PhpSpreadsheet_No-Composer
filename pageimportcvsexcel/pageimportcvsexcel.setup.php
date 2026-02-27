948e0064f83da1e470765f53a2d1cfb13f890b99
sodium carbonate <webitproff@gmail.com>
2026-02-27 08:41:39 +0200

    Bump version to 2.2.27 and update metadata
    Updated version number and metadata for the pageimportcvsexcel plugin.

--- pageimportcvsexcel/pageimportcvsexcel.setup.php
+++ pageimportcvsexcel/pageimportcvsexcel.setup.php
@@ -5,8 +5,8 @@ Code=pageimportcvsexcel
 Name=Page Module articles Import from CSV/Excel
 Category=tools
 Description=Plugin for importing data to Page Module from Excel files into Cotonti using PhpSpreadsheet
-Version=2.0.1
-Date=Feb 25Th, 2026
+Version=2.2.27
+Date=Feb 27Th, 2026
 Author=webitproff
 Copyright=(c) webitproff 2026 | https://github.com/webitproff
 Notes=
@@ -32,15 +32,16 @@ use_function_log=05:radio::0:Использовать функцию логир�
  * Plugin pageimportcvsexcel for Cotonti 0.9.26, PHP 8.4+
  * Filename: pageimportcvsexcel.setup.php
  * Purpose: Setup & Config File. Register data in $db_core, $db_auth and $db_config for the Plugin pageimportcvsexcel
- * Date: Feb 25Th, 2026
+ * Date: Feb 27Th, 2026
  * Source: https://github.com/webitproff/cot-excelimport-PhpSpreadsheet_No-Composer
  * Support: https://abuyfile.com/ru/forums/cotonti/custom/plugs/topic123
  * @package pageimportcvsexcel
- * @version 2.0.1
+ * @version 2.2.27
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
  * @license BSD
  */
 
 defined('COT_CODE') or die('Wrong URL');
+
 
